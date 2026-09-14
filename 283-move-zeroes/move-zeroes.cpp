d42999f5df09a849350fb32dfc492496c7bff8ce
g-Aaryan <guptaaaryan1012@gmail.com>
2026-09-15 02:14:05 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.53%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // Pointer to the first zero
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no zero found, return
+        if (j == -1) return;
+
+         for (int i = j + 1; i < nums.size(); i++) {
+            // If current element is non-zero
+            if (nums[i] != 0) {
+                // Swap with nums[j]
+                swap(nums[i], nums[j]);
+                // Move j to next zero
+                j++;
+            }
+        }
+    }
+};
